2a0dc8b017d46706c3c3992b8c3cf0801305a041
Sergey Vorfolomeev <vorfolomeev@vmssoftware.com>
2019-08-08 09:52:29 +0500

    3

--- vms-ide/src/extension.ts
+++ vms-ide/src/extension.ts
@@ -15,7 +15,7 @@ import {activate as MsgExtension} from "./msg/extension";
 import {activate as PascalExtension} from "./vms_pascal/extension";
 import {activate as Task2CmdExtension} from "./task2cmd/extension";
 import {activate as ZipExtension} from "./zip/extension";
-// import {activate as JvmDebugExtension} from "./vms_jvm_debug/extension";
+import {activate as JvmDebugExtension} from "./vms_jvm_debug/extension";
 
 import {deactivate as deSshHelperExtension} from "./ssh-helper/extension";
 import {deactivate as deSynchronizerExtension} from "./synchronizer/extension";
@@ -25,7 +25,7 @@ import {deactivate as deMsgExtension} from "./msg/extension";
 import {deactivate as dePascalExtension} from "./vms_pascal/extension";
 import {deactivate as deTask2CmdExtension} from "./task2cmd/extension";
 import {deactivate as deZipExtension} from "./zip/extension";
-// import {deactivate as deJvmDebugExtension} from "./vms_jvm_debug/extension";
+import {deactivate as deJvmDebugExtension} from "./vms_jvm_debug/extension";
 
 export async function activate(context: ExtensionContext) {
 
@@ -37,12 +37,12 @@ export async function activate(context: ExtensionContext) {
     await MsgExtension(context);
     await PascalExtension(context);
     await Task2CmdExtension(context);
-    // await JvmDebugExtension(context);
+    await JvmDebugExtension(context);
     SetZipApi(await ZipExtension(context));
 }
 
 export async function deactivate() {
-    // await deJvmDebugExtension();
+    await deJvmDebugExtension();
     await deZipExtension();
     await deTask2CmdExtension();
     await deMsgExtension();
