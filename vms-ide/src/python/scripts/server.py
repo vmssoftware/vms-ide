df3a66353627257a1da80255ced0434c7de4876b
svorfolomeev <vorfol@yandex.ru>
2019-12-13 15:10:54 +0500

    initial scripts

---
+++ vms-ide/src/python/scripts/server.py
@@ -0,0 +1,198 @@
+import sys
+import socket
+import select
+import threading
+import time
+
+HOST = '127.0.0.1'
+PORT = 54326
+CONSOLE = 'CONSOLE'
+DEBUG = 'DEBUG'
+
+class Connection:
+    def __init__(self, socket):
+        self._socket = socket
+        self._recvBuffer = b''
+        self._sendBuffer = b''
+        self._type = None
+    
+    def getType(self):
+        return self._type
+
+    def close(self):
+        try:
+            self._socket.close()
+        except:
+            pass
+        self._socket = None
+
+    def read(self):
+        """ EOL delimited. """
+        self._read()
+        lines = []
+        start = 0
+        while True:
+            idx = self._recvBuffer.find(b"\n", start)
+            if idx == -1:
+                break
+            line = self._recvBuffer[start:idx].decode()
+            # first message is the type
+            if not self._type:
+                self._type = line
+                if self._type == DEBUG:
+                    # send PASUE
+                    self.post('p')
+            elif line:
+                lines.append(line)
+            start = idx + 1
+        self._recvBuffer = self._recvBuffer[start:]
+        return lines
+
+    def post(self, line):
+        """ One command per line. Add EOL at the and """
+        self._sendBuffer += line.encode()
+        self._sendBuffer += b'\n'
+    
+    def write(self):
+        self._write()
+
+    def wantRead(self):
+        return self._socket
+
+    def wantWrite(self):
+        if self._sendBuffer:
+            return self._socket
+        return None
+
+    def _read(self):
+        try:
+            data = self._socket.recv(4096)
+            if data:
+                self._recvBuffer += data
+        except:
+            pass
+
+    def _write(self):
+        if self._sendBuffer:
+            try:
+                sent = self._socket.send(self._sendBuffer)
+                self._sendBuffer = self._sendBuffer[sent:]
+            except:
+                pass
+
+class DebugServer:
+    def __init__(self, port):
+        self._port = port
+        self._listenSocket = None
+        self._connections = None
+        self._thread = None
+        self._stopped = False
+
+    def start(self):
+        if self._thread:
+            self.stop()
+            self.join()
+        self._thread = threading.Thread(target=self._run)
+        self._stopped = False
+        self._thread.start()
+    
+    def stop(self):
+        self._stopped = True
+    
+    def join(self):
+        if self._thread:
+            self._thread.join()
+
+    def isAlive(self):
+        return self._thread != None and self._thread.is_alive()
+    
+    def _run(self):
+        self._listenSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._listenSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self._listenSocket.bind((HOST, self._port))
+        self._listenSocket.listen(2)
+        self._listenSocket.setblocking(False)
+        self._connections = {}
+        try:
+            while not self._stopped:
+                inputs = [self._listenSocket]
+                outputs = []
+                for conn in self._connections.values():
+                    inp = conn.wantRead()
+                    if inp:
+                        inputs.append(inp)
+                    outp = conn.wantWrite()
+                    if outp:
+                        outputs.append(outp)
+                readable, writable, exceptional = select.select(inputs, outputs, inputs)
+                for s in readable:
+                    if s == self._listenSocket:
+                        self._acceptWrapper()
+                    else:
+                        connection = self._connections[s]
+                        lines = connection.read()
+                        if len(lines) > 0:
+                            if connection.getType() == DEBUG:
+                                # print lines
+                                for line in lines:
+                                    print(line)
+                            elif connection.getType() == CONSOLE:
+                                # pass lines to debuggers
+                                for conn in self._connections.values():
+                                    if conn.getType() == DEBUG:
+                                        for line in lines:
+                                            conn.post(line)
+                for s in writable:
+                    if s != self._listenSocket:
+                        self._connections[s].write()
+                for s in exceptional:
+                    if s != self._listenSocket:
+                        self._connections[s].close()
+                        del self._connections[s]
+        except:
+            pass
+        for connection in self._connections.values():
+            try:
+                connection.close()
+            except:
+                pass
+        self._connections = None
+        try:
+            self._listenSocket.close()
+        except:
+            pass
+        self._listenSocket = None
+        self._thread = None
+        self._selector = None
+    
+    def _acceptWrapper(self):
+        conn, addr = self._listenSocket.accept()
+        print("Connected %s" % repr(addr))
+        conn.setblocking(False)
+        connection = Connection(conn)
+        self._connections[conn] = connection
+
+if __name__ == "__main__":
+    server = DebugServer(PORT)
+    server.start()
+    time.sleep(0.5)
+    try:
+        consoleSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        consoleSocket.connect_ex((HOST, PORT))
+        consoleSocket.send((CONSOLE + "\n").encode())
+        while server.isAlive():
+            if sys.version_info[0] < 3:
+                cmd = raw_input('>')
+            else:
+                cmd = input('>')
+            consoleSocket.send((cmd + "\n").encode())
+            if cmd == "q":
+                break
+    except:
+        pass
+    server.stop()
+    try:
+        consoleSocket.send(b"\n")
+    except:
+        pass
+    server.join()
---
+++ vms-ide/src/python/scripts/tracer.py
@@ -0,0 +1,218 @@
+import ctypes
+import signal
+import socket
+import sys
+import threading
+import time
+
+HOST = '127.0.0.1'
+PORT = 54326
+CONSOLE = 'CONSOLE'
+DEBUG = 'DEBUG'
+
+class Tracer:
+    def __init__(self, port):
+        self._port = port
+        self._socket = None
+        self._sendBuffer = b""
+        self._recvBuffer = b""
+        self._lockTrace = threading.Lock()
+        self._oldSysTrace = None
+        self._paused = False
+        self._waitingForAFile = None
+        self._startTracing = False
+        self._threads = {}
+        self._currentThread = threading.current_thread
+        self._sleep = time.sleep
+        self._setTrace = sys.settrace
+        self._setThreadTrace = threading.settrace
+        self._versionInfo = sys.version_info
+        self._PAUSED = 'PAUSED'
+        self._EXITED = 'EXITED'
+        self._CONTUNUED = 'CONTUNUED'
+        self._STEPPED = 'STEPPED'
+        self._INFO = 'INFO'
+        self._fileName = __file__
+
+    def _setupTrace(self):
+        self._connect()
+        self._oldSysTrace = sys.gettrace()
+        self._setTrace(self._traceFunc)
+        self._setThreadTrace(self._traceFunc)
+
+    def _connect(self):
+        self._open()
+        self._sendDbgMessage(DEBUG)
+
+    def _disconnect(self):
+        self._sendDbgMessage(self._EXITED)
+        self._close()
+
+    def _open(self):
+        self._socket = None
+        try:
+            self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self._socket.connect((HOST, self._port))
+            self._socket.setblocking(False)
+        except:
+            # print('Connect failed')
+            self._socket = None
+    
+    def _close(self):
+        try:
+            self._socket.close()
+        except:
+            pass
+        finally:
+            self._socket = None
+
+    def _cleanupTrace(self):
+        self._setTrace(self._oldSysTrace)
+        self._setThreadTrace(self._oldSysTrace)
+        self._disconnect()
+
+    # def _signalHandler(self, signum, frame):
+    #     self._sendDbgMessage('Caught signal %d' % signum)
+    #     self._paused = False
+    
+    def _isConnected(self):
+        return bool(self._socket)
+
+    def _sendDbgMessage(self, message):
+        """ Also add EOL at the end of message """
+        self._sendBuffer += message.encode()
+        self._sendBuffer += b'\n'
+        if self._isConnected():
+            try:
+                sent = self._socket.send(self._sendBuffer)
+                self._sendBuffer = self._sendBuffer[sent:]
+            except Exception as ex:
+                if isinstance(ex, IOError):
+                    errno = ex.errno % 1000
+                    if errno != 35:
+                        self._socket = None
+                else:
+                    self._socket = None
+            except:
+                self._socket = None
+
+    def _readDbgMessage(self):
+        if self._isConnected():
+            try:
+                data = self._socket.recv(4096)
+                if data:
+                    self._recvBuffer += data
+                else:
+                    self._close()
+            except Exception as ex:
+                if isinstance(ex, IOError):
+                    errno = ex.errno % 1000
+                    if errno != 35:
+                        self._socket = None
+                else:
+                    self._socket = None
+            except:
+                self._socket = None
+            if self._recvBuffer:
+                idx = self._recvBuffer.find(b"\n")
+                if idx >= 0:
+                    line = self._recvBuffer[:idx].decode()
+                    self._recvBuffer = self._recvBuffer[idx+1:]
+                    return line
+        return None
+
+    def _traceFunc(self, frame, event, arg):
+        if not self._startTracing:
+            return self._traceFunc
+        if frame.f_code.co_filename == self._fileName:
+            return self._traceFunc
+        if self._waitingForAFile:
+            if self._waitingForAFile == frame.f_code.co_filename:
+                self._waitingForAFile = None
+                self._paused = True
+        if not self._waitingForAFile:
+            ident = self._currentThread().ident
+            entry = {'ident': ident, 'frame': frame, 'event': event, 'arg': arg, 'paused': True}
+            self._threads[ident] = entry
+            with self._lockTrace:
+                if self._paused:
+                    self._sendDbgMessage(self._PAUSED)
+                cmd = self._readDbgMessage()
+                if cmd == 'p':
+                    if not self._paused:
+                        self._sendDbgMessage(self._PAUSED)
+                    self._paused = True
+                while self._paused and self._isConnected():
+                    if "c" == cmd:
+                        if self._paused:
+                            self._sendDbgMessage(self._CONTUNUED)
+                        self._paused = False
+                    elif "s" == cmd:
+                        self._sendDbgMessage(self._STEPPED)
+                        break
+                    elif "i" == cmd:
+                        self._sendDbgMessage(self._INFO)
+                        self._sendDbgMessage("Threads: %i" % len(self._threads))
+                        for threadEntry in self._threads.values():
+                            self._sendDbgMessage("  thread %i%s:" % (threadEntry['ident'], " paused" if threadEntry['paused'] else ""))
+                            self._sendDbgMessage("      file: %s" % threadEntry['frame'].f_code.co_filename)
+                            self._sendDbgMessage("      line: %i" % threadEntry['frame'].f_lineno)
+                    self._sleep(0.3)
+                    cmd = self._readDbgMessage()
+            entry['paused'] = False
+        return self._traceFunc
+
+    def _runscript(self, filename):
+        # === Given from PDB.PY ===
+        import __main__
+        builtinsT = __builtins__
+        __main__.__dict__.clear()
+        __main__.__dict__.update({"__name__"    : "__main__",
+                                  "__file__"    : filename,
+                                  "__builtins__": builtinsT,
+                                 })
+        self._waitingForAFile = filename
+        globalsT = __main__.__dict__
+        try:
+            if self._versionInfo[0] < 3:
+                self._startTracing = True
+                execfile(filename, globalsT, globalsT)
+            else:
+                with open(filename, "rb") as fp:
+                    statement = "exec(compile(%r, %r, 'exec'))" % (fp.read(), filename)
+                self._startTracing = True
+                exec(statement, globalsT, globalsT)
+        except Exception as ex:
+            print(ex)
+    
+    def run(self, filename):
+        self._setupTrace()
+        self._runscript(filename)
+        self._cleanupTrace()
+
+#===================================================================
+
+if __name__ == "__main__":
+
+    _usage = """\
+usage: deb.py [-c command] ... [-m module | pyfile] [arg] ...
+
+Debug the Python program given by pyfile."""
+
+    import getopt
+
+    opts, args = getopt.getopt(sys.argv[1:], 'h:', ['help'])
+
+    if not args:
+        print(_usage)
+        sys.exit(2)
+
+    for opt, optarg in opts:
+        if opt in ['-h', '--help']:
+            print(_usage)
+            sys.exit()
+        else:
+            print("Unlnown option %s" % opt)
+
+    tracer = Tracer(PORT)
+    tracer.run(args[0])
